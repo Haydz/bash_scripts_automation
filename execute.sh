fab34a666d2d66a949c1453ce866a09731e28f70
Haydz <haydnit@gmail.com>
2022-12-18 20:13:28 -0500

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 # execute with git clone https://github.com/Haydz/bash_scripts_automation.git  && cd bash_scripts_automation && bash execute.sh
-su -c "echo 'hello' >>/home/haydnuser/test4.txt" haydnuser
+su -c "echo 'hello' >>/home/haydnuser/fromgh.txt" haydnuser
