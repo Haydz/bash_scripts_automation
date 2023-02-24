244dd0bd5b9fc515c94743a10e233b89aea4e4d1
Haydz <haydnit@gmail.com>
2023-02-24 14:34:57 -0500

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
+# - Testing pulling a repo and executing shell script as a different users
 # execute with git clone https://github.com/Haydz/bash_scripts_automation.git  && cd bash_scripts_automation && bash execute.sh
 su -c "echo 'hello' >>/home/haydnuser/fromgh.txt" haydnuser
